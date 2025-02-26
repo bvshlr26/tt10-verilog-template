fd1865d6fee8cda102a9631f0c4558930d47348e
bvshlr26 <bvshlr26@gmail.com>
2025-02-26 16:10:03 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -10,7 +10,7 @@ async def test_project(dut):
     dut._log.info("Start")
 
     # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, units="us")
+    clock = Clock(dut.clk, 1, units="us")
     cocotb.start_soon(clock.start())
 
     # Reset
@@ -48,3 +48,14 @@ async def test_project(dut):
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
     assert dut.uo_out.value == 40
+
+# Set the input values you want to test
+    dut.ui_in.value = 250
+    dut.uio_in.value = 50
+
+    # Wait for one clock cycle to see the output values
+    await ClockCycles(dut.clk, 1)
+
+    # The following assersion is just an example of how to check the output values.
+    # Change it to match the actual expected output of your module:
+    assert dut.uo_out.value == 0
